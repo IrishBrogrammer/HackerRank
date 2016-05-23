bbe3e340367bc6b469788d7d28fa5fcd17f8acf2
Brian McAleese <bmcaleese12@gmail.com>
2016-05-23 15:30:55 +0100

    Marking coloumns and rows for a given layer correctly

--- MatrixRotation/rotation.py
+++ MatrixRotation/rotation.py
@@ -5,25 +5,38 @@ def printMatrix( matrix ) :
     for row in matrix : 
         print row
             
-def CreateTextMatrix( size ) : 
+def CreateTextMatrix( xSize , ySize ) : 
     matrix = []
     
-    for x in range( 0 , 5 ) :
+    for x in range( 0 , xSize ) :
         matrix.append( [] ) 
-        for y in range( 0 , size ) :
+        for y in range( 0 , ySize ) :
             matrix[x].append("x")    
     
     return matrix
  
+ 
+def ShouldStop( min , max ) :
+    return min >= max
 
 
 def MarkDepths( startX , endX , startY , endY , depth , matrix ) :
+    xSize = endX - startX
+    ySize = endY - startY 
     
-    if  startX >= endX  :
+    stop = False    
+    if ( xSize < ySize ) :
+        stop = ShouldStop( startX , endX )
+    else : 
+        stop = ShouldStop( startY , endY )
+    
+    if  stop :
         return matrix
     else :
         matrix[startY] = MarkRow( matrix[startY] ,  startX , endX , str(depth) )
         matrix[endY] = MarkRow( matrix[endY] ,startX , endX , str( depth ) )
+        matrix = MarkColumn( startX , startY , endY , matrix , str(depth ) )
+        matrix = MarkColumn( endX - 1 , startY , endY  , matrix , str(depth ) )
         matrix = MarkDepths( startX + 1 , endX - 1 , startY + 1 , endY - 1 , depth + 1 , matrix)
         return matrix
         
@@ -33,6 +46,13 @@ def MarkRow( row , startX , endX  , val ) :
         row[x] = val     
     return row
 
+def MarkColumn( xVal , startY , endY , matrix , val ) :
+    for col in range( startY , endY ) :
+        matrix[col][xVal] = val
+    return matrix 
+    
+    
+    
 def MapoutMatrix( matrixToMap ) :
     matrixToMap = MarkDepths( 0 , 4 , 0 , 4 , 1, matrixToMap )
     
@@ -40,7 +60,7 @@ def MapoutMatrix( matrixToMap ) :
 
 
 
-testMatrix = CreateTextMatrix( 4 )
+testMatrix = CreateTextMatrix( 5 , 4  )
 testMatrix = MapoutMatrix( testMatrix)
 printMatrix( testMatrix )
 
